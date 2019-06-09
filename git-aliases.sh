2286d0e05e91176b8b26b4a73c769bf0f95565df
ilya-kuznetsov <et.anchoret@gmail.com>
2019-06-10 00:18:03 +0300

    File git-aliases.sh was added

---
+++ git-aliases.sh
@@ -0,0 +1,14 @@
+git config --global alias.ch checkout
+git config --global alias.br branch
+git config --global alias.cm commit
+git config --global alias.st status
+git config --global alias.pl pull
+git config --global alias.ph push
+git config --global alias.mr merge
+git config --global alias.rb rebase
+git config --global alias.rbc 'rebase --continue'
+git config --global alias.rbs 'rebase --skip'
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.uns 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+git config --global alias.gr 'log --pretty=oneline --graph --abbrev-commit'
